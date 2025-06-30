8a6e615216a16dc64ae0e7af4383f4bdfcd53222
YuvBhatt-Dev <204989587+YuvBhatt-YB@users.noreply.github.com>
2025-07-01 02:23:26 +0530

    Fix template folder case sensitivity for Render

--- RoastHub/blueprints/HomePage/routes.py
+++ RoastHub/blueprints/HomePage/routes.py
@@ -13,7 +13,7 @@ def analyze():
 
     user_resp = get_username(username)
     if user_resp is None:
-        return render_template("Homepage/user_not_found.html")
+        return render_template("HomePage/user_not_found.html")
     
     repos_resp = get_repos_stats(username)
     
@@ -21,7 +21,7 @@ def analyze():
 
     roast_response = call_openrouter_llm(prompt)
     if "error" in roast_response:
-        return render_template("Homepage/error.html",message=roast_response["error"])
+        return render_template("HomePage/error.html",message=roast_response["error"])
     
     
     return render_template("HomePage/result.html",username=username,user_resp=user_resp,user_data=user_resp["user_stats"],repos = repos_resp["repos"],roast=roast_response)
